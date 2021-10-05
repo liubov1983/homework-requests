4e0aaa8d6c2e025aa09d6a4456c756337a284883
Liubov Yakovleva <regnum1983@yandex.ru>
2021-10-05 21:02:18 +0300

    update task2

--- task2.py
+++ task2.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 class YaUploader:
     def __init__(self, token):
@@ -12,21 +11,23 @@ class YaUploader:
         }
 
     def _get_upload_link(self, file_path):
+        file_name = file_path.split('\\')[-1]
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': file_path, 'overwrite': 'True'}
+        params = {'path': file_name, 'overwrite': 'True'}
         response = requests.get(url=upload_url, headers=headers, params=params)
         return response.json()
 
     def upload(self, file_path):
         href = self._get_upload_link(file_path=file_path).get('href', '')
-        response = requests.put(href, data=open('test.txt', 'rb'))
-        response.raise_for_status()
-        if response.status_code == 201:
-            print("Файл успешно загружен")
+        with open(file_path, 'rb') as f:
+            response = requests.put(href, files={'file': f})
+            response.raise_for_status()
+            if response.status_code == 201:
+                print("Файл успешно загружен")
 
 if __name__ == '__main__':
-    uploader = YaUploader('')
-    result = uploader.upload('test.txt')
+    uploader = YaUploader('AQAAAAAVUMzsAADLW07-JJToAEgZjpwrFv4C5NU')
+    result = uploader.upload('C:\Web-разработка\Нетология\some_text.txt')
 
     
