4019060ca1e039ba59b819c66f3c5d96a58c29a9
Liubov Yakovleva <regnum1983@yandex.ru>
2021-10-05 18:39:48 +0300

    complete task2

---
+++ task2.py
@@ -0,0 +1,33 @@
+import requests
+from pprint import pprint
+
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': file_path, 'overwrite': 'True'}
+        response = requests.get(url=upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path):
+        href = self._get_upload_link(file_path=file_path).get('href', '')
+        response = requests.put(href, data=open('test.txt', 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Файл успешно загружен")
+
+if __name__ == '__main__':
+    uploader = YaUploader('AQAAAAAVUMzsAADLW07-JJToAEgZjpwrFv4C5NU')
+    result = uploader.upload('test.txt')
+
+    
